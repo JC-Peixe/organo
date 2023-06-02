7c0d15202a0a1f9fc16f36be5d9cbbcbe642b7e8
Peixee <Jc_px@hotmail.com>
2023-06-02 15:01:29 -0300

    corrigindo o mudarCor
    ainda não muda a cor, mas pelo menos não diz que função não existe

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,24 +1,37 @@
-import Colaborador from '../Colaborador'
-import './Time.css'
-
-const Time = ({ time, colaborador, aoDeletar, mudarCor }) => {
-    // const css = {backgroundColor:time.corSecundaria}
-    return (
-        time.colaboradores.length > 0 && <section className='time' style={{ backgroundColor: time.corSecundaria }} >
-            <input onChange={evento => mudarCor(evento.target.value, time.nome)} value={time.corSecundaria} type='color' className='input-cor' />
-            {/* (props.colaboradores.length > 0) ? <section className='time' style={css} > */}
-            <h3 style={{ borderColor: time.corPrimaria }}>
-                {time.nome}
-            </h3>
-            <div className='colaboradores'>
-                {time.colaboradores.map(colaborador => {
-
-                    return <Colaborador corDeFundo={time.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} aoDeletar={aoDeletar} />
-                })}
-            </div>
-        </section>
-        // :''
+import Colaborador from '../Colaborador';
+import './Time.css';
+ 
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
+  // const css = {backgroundColor:time.corSecundaria}
+  return (
+    colaboradores.length > 0 && (
+      <section className="time" style={{ backgroundColor: time.corSecundaria }}>
+        <input
+          onChange={(evento) => mudarCor(evento.target.value, time.nome)}
+          value={time.corSecundaria}
+          type="color"
+          className="input-cor"
+        />
+        {/* (props.colaboradores.length > 0) ? <section className='time' style={css} > */}
+        <h3 style={{ borderColor: time.corPrimaria }}>{time.nome}</h3>
+        <div className="colaboradores">
+          {colaboradores.map((colaborador) => {
+            return (
+              <Colaborador
+                corDeFundo={time.corPrimaria}
+                key={colaborador.nome}
+                nome={colaborador.nome}
+                cargo={colaborador.cargo}
+                imagem={colaborador.imagem}
+                aoDeletar={aoDeletar}
+              />
+            );
+          })}
+        </div>
+      </section>
     )
-}
-
-export default Time
+    // :''
+  );
+};
+ 
+export default Time;
